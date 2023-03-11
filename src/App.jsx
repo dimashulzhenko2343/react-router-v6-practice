5f09f389908dc2ec52abf9ec5a61e87add4e53b1
dimaShulzhenko <dimashulzhenko2343@gmail.com>
2023-03-11 17:01:29 +0300

    Clear base template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+        react-router-v6-practice
+    </div>
+  )
+}
+
+export default App;
